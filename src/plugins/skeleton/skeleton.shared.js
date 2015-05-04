64a8310cf950f215f4db63c0264314bea70e894d
Aviat Networks <jay.shepherd@aviatnet.com>
2015-05-04 19:35:34 +1200

    Skeleton table added extra command

--- src/plugins/skeleton/skeleton.shared.js
+++ src/plugins/skeleton/skeleton.shared.js
@@ -1,7 +1,7 @@
 var SKELETON = (function(skeleton){
 	
 	skeleton.CHANNEL_ID = "SKELETON";
-	skeleton.COMMANDS = {GET_TIME: 0};
+	skeleton.COMMANDS = {GET_TIME: 0, COMMAND2: 1};
 	
 	return skeleton;
 }(SKELETON || {}));
