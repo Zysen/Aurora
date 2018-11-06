546b4b1989160c5202f2742958d3b43e51bda12d
Eduard Vaks <eddie.vaks@aviatnet.com>
2018-11-07 07:16:14 +1300

    CT-4340 add log management

--- plugins/config/config.server.js
+++ plugins/config/config.server.js
@@ -13,18 +13,22 @@ config = (function() {
 
     fs.watchFile(configFilePath, {interval: 500, persistent: true}, function(curr, prev) {
         fs.readFile(configFilePath, function(err, configFile) {
-            var newConfig = JSON.parse(configFile.toString());
-            for (var index in newConfig) {
-                pub[index] = newConfig[index];
+            try {
+                var newConfig = JSON.parse(configFile.toString());
+                for (var index in newConfig) {
+                    pub[index] = newConfig[index];
+                }
+                aurora.object.deepDiff(new Object(lastConfig), new Object(newConfig)).forEach(function(diff) {
+                    pub.configE.emit(diff.path.join('/'), diff, true);
+                });
+                lastConfig = newConfig;
+                
+                if (newConfig.aurora && newConfig.aurora.title !== undefined) {
+                    process.title = config.aurora.title;
+                }
             }
-            aurora.object.deepDiff(new Object(lastConfig), new Object(newConfig)).forEach(function(diff) {
-                console.log('Config Change ' + diff.path.join('/'));
-                pub.configE.emit(diff.path.join('/'), diff, true);
-            });
-            lastConfig = newConfig;
-
-            if (newConfig.aurora.title !== undefined) {
-                process.title = config.aurora.title;
+            catch (e) {
+                console.error("error reading config file", e);
             }
         });
     });
