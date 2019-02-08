2c49896d6d0d0e00979cd475cc227bc6b5866707
Eduard Vaks <eddie.vaks@aviatnet.com>
2019-02-08 14:06:00 +1300

    CT-6217 TextDecoder does not exist on Edge

--- plugins/websocket/websocket.client.js
+++ plugins/websocket/websocket.client.js
@@ -25,8 +25,47 @@ function convertData(data) {
     }
 }
 
+function Utf8ArrayToStr(array) {
+    var out, i, len, c;
+    var char2, char3;
+    
+    out = "";
+    len = array.length;
+    i = 0;
+    while(i < len) {
+        c = array[i++];
+        switch(c >> 4)
+        { 
+        case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+            // 0xxxxxxx
+            out += String.fromCharCode(c);
+            break;
+        case 12: case 13:
+                    // 110x xxxx   10xx xxxx
+            char2 = array[i++];
+            out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+            break;
+        case 14:
+            // 1110 xxxx  10xx xxxx  10xx xxxx
+            char2 = array[i++];
+            char3 = array[i++];
+            out += String.fromCharCode(((c & 0x0F) << 12) |
+                                       ((char2 & 0x3F) << 6) |
+                                       ((char3 & 0x3F) << 0));
+            break;
+        }
+    }
+            
+    return out;
+}
+
 function arrayBufferToString(ab) {
-    return new TextDecoder("utf-8").decode(new Uint8Array(ab));
+    if (window.TextDecoder) {
+        return new TextDecoder("utf-8").decode(new Uint8Array(ab));
+    }
+    else {
+        return Utf8ArrayToStr(new Uint8Array(ab));
+    }
 }
 
 function toUInt16ArrayBuffer(data, littleEndian) {
